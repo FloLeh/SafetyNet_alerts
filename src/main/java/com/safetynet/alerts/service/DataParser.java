74cc0173150f4f4b37a86132e46433d3604d4100
FloLeh <florianlehuede@gmail.com>
2025-02-19 16:21:18 +0100

    parse data from JSON

--- src/main/java/com/safetynet/alerts/service/DataParser.java
+++ src/main/java/com/safetynet/alerts/service/DataParser.java
@@ -1,24 +1,29 @@
 package com.safetynet.alerts.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.safetynet.alerts.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class DataParser implements CommandLineRunner {
+
+    @Autowired
+    PersonService personService;
 
     @Override
     public void run(String... args) throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
-        HashMap<String, Iterable<Person>> map = mapper.readValue(new File("src/main/resources/data.json"), HashMap.class);
+        HashMap<String, List<HashMap<String, String>>> map = mapper.readValue(new File("src/main/resources/data.json"), HashMap.class);
 
-        Iterable<Person> persons = map.get("persons");
-        System.out.println(persons);
+        List<HashMap<String, String>> persons = map.get("persons");
+
+        persons.forEach(personService::savePerson);
 
     }
 }
--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Data
 @Service
 public class PersonService {
@@ -15,5 +17,20 @@ public class PersonService {
 
     public Iterable<Person> getPersons() {
         return personRepository.findAll();
+    }
+
+    public Person savePerson(HashMap<String, String> input) {
+
+        Person person = new Person();
+
+        person.setFirstName(input.get("firstName"));
+        person.setLastName(input.get("lastName"));
+        person.setAddress(input.get("address"));
+        person.setCity(input.get("city"));
+        person.setZip(input.get("zip"));
+        person.setPhone(input.get("phone"));
+        person.setEmail(input.get("email"));
+
+        return personRepository.save(person);
     }
 }
